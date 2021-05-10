4fec1d342c6a98999b9d75745b2e1a54105046bd
Khanali P <xanali.panahov1994@gmail.com>
2021-05-10 12:56:27 +0400

    rename title

--- src/app/store.js
+++ src/app/store.js
@@ -6,4 +6,3 @@ export default configureStore ({
         user: userReducer
     },
 })
-
--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -3,23 +3,23 @@ import { createSlice } from "@reduxjs/toolkit";
 const userSlice = createSlice({
     name: "user",
     initialState: {
-    isSignedIn: false,
-    userData: null,
-    searchInput: "tech",
-    blogData: null,
+        isSignedIn: false,
+        userData: null,
+        searchInput: "tech",
+        blogData: null,
     },
     reducers: {
         setSignedIn: (state, action) => {
-        state.isSignedIn = action.payload;
+            state.isSignedIn = action.payload;
         },
         setUserData: (state, action) => {
-        state.userData = action.payload;
+            state.userData = action.payload;
         },
         setInput: (state, action) => {
-        state.searchInput = action.payload;
+            state.searchInput = action.payload;
         },
         setBlogData: (state, action) => {
-        state.blogData = action.payload;
+            state.blogData = action.payload;
         },
     },
 });
